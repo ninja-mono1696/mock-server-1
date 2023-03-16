2029ac24d644736d63250410a8d539e4e1a80167
Monojit Mondal <mondalmonojit1696@gmail.com>
2023-03-16 13:24:43 +0530

    updated the server file

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 // Add this before server.use(router)
 
+server.use(
+  // Add custom route here if needed
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+
 server.use(router);
 
 // Listen to port
